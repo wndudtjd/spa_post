98eca62f93ae07443fd5c53998b59264106c3db6
wndudtjd <dudtjdwn1575@naver.com>
2023-02-28 10:19:45 +0900

    router 새로 업데이트!

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const postsRouter = require("./posts");
 // const commentsRouter = require("./comments");
 
-const routes = express.Router();
+const router = express.Router();
 
-routes.use('/posts', postsRouter);
+router.use('/posts', postsRouter);
 // routes.use('/comments', commentsRouter);
 
-module.exports = routes;
+module.exports = router;
